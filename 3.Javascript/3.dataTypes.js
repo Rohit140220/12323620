143762a81e59d466b1799529667623787c5b50db
= <shivam.webcraft@gmail.com>
2026-01-27 12:09:06 +0530

    data types

---
+++ 3.Javascript/3.dataTypes.js
@@ -0,0 +1,45 @@
+// 8.object:
+let obj = { name: "alice" };
+console.log(obj);
+
+// 7.symbol: It is a primitive data type and it creates unique and immutable values, primarily used as an identifiers for object properties keys:
+let sym = Symbol("id");
+console.log(sym);
+console.log(typeof sym);
+
+// 6.boolean:
+let p = false;
+console.log(p);
+console.log(typeof p);
+
+// 5.bigint:
+let bigInt = 12323435432453243214323123n;
+console.log(bigInt);
+console.log(typeof bigInt);
+
+// 4.null: type is object
+let j = null;
+console.log(j);
+console.log(typeof j);
+
+// 3.undefined
+let y;
+console.log(y);
+console.log(typeof y);
+y = 20;
+console.log(y);
+
+// 2.string:
+let x = "Shiva";
+console.log(x);
+console.log(typeof x);
+
+// 1.number:
+let a = 10;
+let b = -5;
+let c = 2.5;
+console.log(a);
+console.log(b);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
