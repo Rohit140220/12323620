fabba9dcfdd8df926808202807e53e9c631d862c
= <shivam.webcraft@gmail.com>
2026-01-29 11:26:36 +0530

    equal coercion

---
+++ 3.Javascript/12.equal_coercion.js
@@ -0,0 +1,11 @@
+console.log(1 == 1);
+console.log(1 == "1");
+console.log(1 === "1");
+console.log(1 === "A");
+console.log(1 == "A");
+console.log(1 == []);
+console.log(0 == []);
+console.log(1 == "");
+console.log(1 == " ");
+console.log(1 == " ");
+console.log([] == "");
